f5bdb95a19b0f42757e01eb61442d7d848707a63
Solomon-kassahun <Solomon.kassahun2@gmail.com>
2016-07-11 14:58:25 -0500

    Category and Order classes commit

---
+++ src/main/java/com/cs545/ecom/domain/Order.java
@@ -0,0 +1,134 @@
+/**
+ * 
+ */
+package com.cs545.ecom.domain;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Solomon Kassahun
+ *
+ */
+public class Order {
+	private String orderId;
+	private LocalDate orderDate;
+	private User user;
+	private BigDecimal orderPrice;
+	private List<OrderItem> orderItems;
+	
+	/**
+	 * Default no-arg constructor
+	 */
+	public Order() {
+		super();
+		orderItems = new ArrayList<OrderItem>();
+	}
+
+	/**
+	 * @return the orderId
+	 */
+	public String getOrderId() {
+		return orderId;
+	}
+
+	/**
+	 * @param orderId the orderId to set
+	 */
+	public void setOrderId(String orderId) {
+		this.orderId = orderId;
+	}
+
+	/**
+	 * @return the orderDate
+	 */
+	public LocalDate getOrderDate() {
+		return orderDate;
+	}
+
+	/**
+	 * @param orderDate the orderDate to set
+	 */
+	public void setOrderDate(LocalDate orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	/**
+	 * @return the user
+	 */
+	public User getUser() {
+		return user;
+	}
+
+	/**
+	 * @param user the user to set
+	 */
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	/**
+	 * @return the orderPrice
+	 */
+	public BigDecimal getOrderPrice() {
+		return orderPrice;
+	}
+
+	/**
+	 * @param orderPrice the orderPrice to set
+	 */
+	public void setOrderPrice(BigDecimal orderPrice) {
+		this.orderPrice = orderPrice;
+	}
+
+	/**
+	 * @return the orderItems
+	 */
+	public List<OrderItem> getOrderItems() {
+		return orderItems;
+	}
+
+	/**
+	 * @param orderItems the orderItems to set
+	 */
+	public void setOrderItems(List<OrderItem> orderItems) {
+		this.orderItems = orderItems;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((orderId == null) ? 0 : orderId.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		if (orderId == null) {
+			if (other.orderId != null)
+				return false;
+		} else if (!orderId.equals(other.orderId))
+			return false;
+		return true;
+	}
+	
+	
+	
+
+}
